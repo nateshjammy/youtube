6d98a0f7cfbba88b69da69cd6f082aa1ff4fe9ca
nateshjammy <42469377+nateshjammy@users.noreply.github.com>
2018-10-10 14:30:26 +0530

    Initial commit

---
+++ app/src/main/java/salesnotification/honda/tansi/com/youtube/Config.java
@@ -0,0 +1,10 @@
+package salesnotification.honda.tansi.com.youtube;
+
+public class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAKaIDCwGIo-p2JzOSQYm0Oz7lJvLS6Yjw";
+
+}
